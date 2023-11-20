72fca8aa10300eb9bdc07eec16d57d3f3fc69263
graycat27 <64903817+graycat27@users.noreply.github.com>
2023-11-21 05:19:37 +0900

    implement

--- src/main/java/org/maxgamer/quickshop/economy/Economy_JpsCore.java
+++ src/main/java/org/maxgamer/quickshop/economy/Economy_JpsCore.java
@@ -1,5 +1,15 @@
 package org.maxgamer.quickshop.economy;
 
+import gg.jps.jpscore.JpsCore;
+import gg.jps.jpscore.api.JpsApiResponse;
+import gg.jps.jpscore.api.i.IJpsBankApi;
+import gg.jps.jpscore.core.JpsUtils;
+import gg.jps.jpscore.db.def.strChar.PlayerUuidStr;
+import gg.jps.jpscore.db.def.varchar.PassBookNote;
+import gg.jps.jpscore.db.def.varchar.PluginName;
+import gg.jps.jpscore.define.JpsConst;
+import gg.jps.jpscore.define.exception.JpsApiException;
+import gg.jps.jpscore.economy.def.MoneyJp;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.plugin.Plugin;
@@ -14,70 +24,125 @@ public class Economy_JpsCore extends AbstractEconomy {
 
     private static final QuickShop plugin = QuickShop.getInstance();
 
+    private final IJpsBankApi api = JpsCore.API().JpsBankApi();
+
+    private String lastError = null;
     @Override
-    public boolean deposit(@NotNull UUID name, double amount, @NotNull World world, @Nullable String currency) {
-        //TODO implement
-        return false;
+    public @Nullable String getLastError() {
+        return this.lastError;
+    }
+
+    @Override
+    public boolean deposit(@NotNull UUID toPlayer, double amount, @NotNull World world, @Nullable String currency) {
+        try {
+            JpsApiResponse<Boolean> res = api.giveBySystem(
+                    new PlayerUuidStr(toPlayer), PluginName.instance(plugin.getName()), new MoneyJp(amount),
+                    PassBookNote.instance("ショップ収入"), IJpsBankApi.AddableToDailyEarnAmount.YES
+            );
+            return res.get();
+        }catch(JpsApiException e){
+            plugin.getLogger().warning(e.getMessage());
+            JpsUtils.Logging.fail(e.getCause());
+            lastError = e.getCause().getMessage();
+            return false;
+        }
     }
 
     @Override
     public boolean deposit(@NotNull OfflinePlayer trader, double amount, @NotNull World world, @Nullable String currency) {
-        //TODO implement
-        return false;
+        try {
+            JpsApiResponse<Boolean> res = api.giveBySystem(
+                PlayerUuidStr.instance(trader), PluginName.instance(plugin.getName()), new MoneyJp(amount),
+                PassBookNote.instance("ショップ収入"), IJpsBankApi.AddableToDailyEarnAmount.YES
+            );
+            return res.get();
+        }catch(JpsApiException e){
+            plugin.getLogger().warning(e.getMessage());
+            JpsUtils.Logging.fail(e.getCause());
+            lastError = e.getCause().getMessage();
+            return false;
+        }
     }
 
     @Override
     public String format(double balance, @NotNull World world, @Nullable String currency) {
-        //TODO implement
-        return null;
+        return new MoneyJp(balance).toUnitString();
     }
 
     @Override
     public double getBalance(@NotNull UUID name, @NotNull World world, @Nullable String currency) {
-        //TODO implement
-        return 0;
+        try{
+            JpsApiResponse<MoneyJp> res = api.currentCharge(api.getAccountId(new PlayerUuidStr(name)).get());
+            return res.get().doubleValue();
+        }catch(JpsApiException e){
+            plugin.getLogger().warning(e.getMessage());
+            JpsUtils.Logging.fail(e.getCause());
+            return 0;
+        }
     }
 
     @Override
     public double getBalance(@NotNull OfflinePlayer player, @NotNull World world, @Nullable String currency) {
-        //TODO implement
-        return 0;
+        try{
+            JpsApiResponse<MoneyJp> res = api.currentCharge(api.getAccountId(PlayerUuidStr.instance(player)).get());
+            return res.get().doubleValue();
+        }catch(JpsApiException e){
+            plugin.getLogger().warning(e.getMessage());
+            JpsUtils.Logging.fail(e.getCause());
+            return 0;
+        }
     }
 
     @Override
-    public boolean withdraw(@NotNull UUID name, double amount, @NotNull World world, @Nullable String currency) {
-        //TODO implement
-        return false;
+    public boolean withdraw(@NotNull UUID fromPlayer, double amount, @NotNull World world, @Nullable String currency) {
+        try {
+            JpsApiResponse<Boolean> res = api.tollBySystem(
+                    new PlayerUuidStr(fromPlayer), PluginName.instance(plugin.getName()),
+                    new MoneyJp(amount), PassBookNote.instance("ショップ費用")
+            );
+            return res.get();
+        }catch(JpsApiException e){
+            plugin.getLogger().warning(e.getMessage());
+            JpsUtils.Logging.fail(e.getCause());
+            lastError = e.getCause().getMessage();
+            return false;
+        }
     }
 
     @Override
     public boolean withdraw(@NotNull OfflinePlayer trader, double amount, @NotNull World world, @Nullable String currency) {
-        //TODO implement
-        return false;
+        try {
+            JpsApiResponse<Boolean> res = api.tollBySystem(
+                    PlayerUuidStr.instance(trader), PluginName.instance(plugin.getName()),
+                    new MoneyJp(amount), PassBookNote.instance("ショップ費用")
+            );
+            return res.get();
+        }catch(JpsApiException e){
+            plugin.getLogger().warning(e.getMessage());
+            JpsUtils.Logging.fail(e.getCause());
+            lastError = e.getCause().getMessage();
+            return false;
+        }
     }
 
     @Override
     public boolean hasCurrency(@NotNull World world, @NotNull String currency) {
-        //TODO implement
         return false;
     }
 
     @Override
     public boolean supportCurrency() {
-        //TODO implement
         return false;
-    }
-
-    @Override
-    public @Nullable String getLastError() {
-        //TODO implement
-        return null;
     }
 
     @Override
     public boolean isValid() {
-        //TODO implement
-        return false;
+        return api != null;
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return JpsConst.pluginName.get();
     }
 
     @Override
@@ -87,7 +152,6 @@ public class Economy_JpsCore extends AbstractEconomy {
 
     @Override
     public String toString() {
-        //TODO implement
-        return null;
+        return getClass().getName() +":{isValid:"+ isValid() +", lastError:"+ lastError +"}";
     }
 }
